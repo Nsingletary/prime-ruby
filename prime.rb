befbc1bcd6c2cdb6c50936d83afdea22d2f0a0f5
Nsingletary <singletarybush@gmail.com>
2020-11-21 15:15:32 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+
+iterations = 10
+
+def prime?(integer)
+  return false if integer <= 1
+  Math.sqrt(integer).to_i.downto(2).each do |divisor|
+    return false if integer % divisor == 0
+  end
+  true
+end
